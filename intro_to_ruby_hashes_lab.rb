995fe515315c5643e991041cc2d078da19bb7f11
Shemar Dacosta <dshemar6@gmail.com>
2019-01-08 02:11:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def monopoly_with_third_tier
 				two_pieces_owned: 50,
 				three_pieces_owned: 100,
 				four_pieces_owned: 200
-				
+
 			},
 			names: {
 				reading_railroad: {},
@@ -57,7 +57,7 @@ def monopoly_with_fourth_tier
 				two_pieces_owned: 50,
 				three_pieces_owned: 100,
 				four_pieces_owned: 200
-				
+
 			},
 			names: {
 				reading_railroad: {
